a35a31c8fc4ef77ceed30ac2c3b0747897496fe9
Joe-PC <joe_rushton@hotmail.co.uk>
2018-02-08 16:44:08 +0000

    Added accounts config file

---
+++ config/accounts.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Twitter Accounts Application Details
+|--------------------------------------------------------------------------
+|
+| The key for each array element wants to be your twitter name - without the @ symbol
+| Then the value wants to be an array containing the oauth access token+secret and consumer key+secret
+| These can be acquired from https://apps.twitter.com
+|
+*/
+return [
+	'accountname' => [
+	    'oauth_access_token' => "",
+	    'oauth_access_token_secret' => "",
+	    'consumer_key' => "",
+	    'consumer_secret' => ""
+	]
+];
